f7fab6da257ae65dac9c253d16ea1c168764cc7f
Jon Holmes <holmjona@isu.edu>
2021-02-04 12:30:36 -0700

    Talked about load / run timing. Also some JavaScript nuances with ===.

--- scripts/intro.js
+++ scripts/intro.js
@@ -1,1 +1,47 @@
-﻿alert("From external");
+﻿//alert("From external");
+
+//arr = [];
+
+//while (arr.length < 11) {
+//    arr.push("steve");
+//    arr[arr.length] = "another";
+//}
+
+//strr = "";
+//for (var looper = 0; looper < arr.length; looper++) {
+//    strr += arr[looper];
+//}
+
+//alert(strr);
+
+var a = 3;
+var b = "3";
+var c = 3.0;
+var d;
+
+//alert([a == b, a == c, b == c]);
+//alert([a === b, a === c, b === c]);
+//alert(
+//    [d == "undefined",
+//    d == undefined]
+//);
+//alert(
+//    [typeof d == "undefined",
+//    typeof d == undefined]
+//    );
+
+//alert(
+//    [typeof d === "undefined",
+//    typeof d === undefined]
+//);
+
+//if ("1" + 2 === 12 ) {
+//    alert("yep");
+//} else {
+//    alert("nope");
+//}
+
+
+
+
+
