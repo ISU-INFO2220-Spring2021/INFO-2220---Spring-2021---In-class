63687cc007d556a3c490c22ba9a475c8e684c679
Jon Holmes <holmjona@isu.edu>
2021-02-09 12:25:57 -0700

    Added functions script and html file for demos.

---
+++ scripts/fun.js
@@ -0,0 +1,19 @@
+﻿
+function apples() {
+    alert("Apples");
+    return "bananas";
+}
+
+var pickle = 6;
+
+pickle = 5;
+
+var bananas = apples;
+
+alert(bananas);
+alert(bananas());
+
+
+function apples(hamburger){
+    alert("hamburger");
+}
