7e664e90e5717f46c6e2f4742395ea40737e731a
Jon Holmes <holmjona@isu.edu>
2021-02-11 12:29:21 -0700

    Added some object stuff.

--- scripts/fun.js
+++ scripts/fun.js
@@ -1,19 +1,67 @@
 ﻿
-function apples() {
-    alert("Apples");
-    return "bananas";
-}
+//function apples() {
+//    alert("Apples");
+//    return "bananas";
+//}
 
-var pickle = 6;
+//var pickle = 6;
 
-pickle = 5;
+//pickle = 5;
 
-var bananas = apples;
+//var bananas = apples;
 
-alert(bananas);
-alert(bananas());
+//alert(bananas);
+//alert(bananas());
 
 
-function apples(hamburger){
-    alert("hamburger");
-}
+//function apples(hamburger){
+//    alert("hamburger");
+//}
+
+//var a = 5;
+//var b = 6;
+
+//var a;
+//var b;
+
+////var x;
+////alert(fuzzyHappyApples);
+////alert(typeof fuzzyHappyApples);
+
+
+//var objI = { a: 1, b: 2 };
+//objI.c = moreFun;
+
+//function moreFun() {
+//    return a + b;
+//};
+
+////alert(moreFun());
+//alert(objI.c());
+
+
+function Animal(shout) {
+    this.hasFur = false;
+    this.name = "";
+    this.sound = shout;
+    return shout ;
+} 
+
+var ani = new Animal("growl");
+
+ani.hasFur = true;
+ani.legs = 4;
+
+alert(Animal("bark"));
+
+
+var zoo = [];
+zoo.push(new Animal());
+zoo.push(new Animal());
+zoo.push(new Animal());
+zoo.push(new Animal());
+zoo.push(new Animal());
+zoo.push(new Animal());
+zoo.push(new Animal());
+
+
