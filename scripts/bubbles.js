8affde64da5dbcdadae7bc90293ae4cb1788b636
Jon Holmes <holmjona@isu.edu>
2021-03-02 12:49:36 -0700

    Pre midterm Discussion

---
+++ scripts/bubbles.js
@@ -0,0 +1,39 @@
+﻿window.onload = function() {
+    var obj = document.getElementById("art1").parentNode; // Section
+    obj.onclick = function (e) {
+        e = e || window.event; // always include this line.
+        var tar = e.target || e.srcElement; // gets waht was clicked
+        var cTar = e.currentTarget;
+        if (tar.tagName === "P") {
+            alert(tar.outerHTML + " is a Paragraph in" + cTar.tagName);
+        } else {
+            alert(tar.outerHTML + " should fire.");
+        }
+        return false;
+    };
+
+    //obj.onmouseover = function (evt) {
+    //    evt = evt || window.event;
+    //    var tar = evt.target || evt.srcElement;
+    //    //alert(typeof evt);
+    //    if (tar.tagName == "P") {
+    //        tar.className = "hovered";
+    //    }
+    //};
+
+
+    //var paras = document.getElementsByTagName("p");
+    //for (var i = 0; i < paras.length; i++) {
+    //    var par = paras[i];
+    //    par.onmouseover = function () {
+    //        this.className = "hovered";
+    //        //alert("I entered a Paragraph;")
+    //    };
+    //    par.onmouseleave = function () {
+    //        this.className = "";
+    //        //alert("I entered a Paragraph;")
+    //    };
+    //}
+};
+
+
---
+++ scripts/objects-again.js
@@ -0,0 +1,72 @@
+﻿
+function Animal(nme) {
+    this.name = nme;
+    this.age = 2;
+    //alert("Animal was called");
+    //alert("The animal is " + this.age);
+    this.sayName = function() {
+        return this.name;
+    }
+
+    this.soundOff = makeNoise;
+
+    function makeNoise() {
+        return "Growl";
+    }
+}
+
+var dog = new Animal("Fido");
+
+//alert(typeof dog);
+//alert(dog.name);
+dog.name = "Pete";
+//alert(dog.name);
+//alert(dog.sayName());
+//sayYourName(dog);
+//alert(dog.soundOff());
+
+function sayYourName(ani) {
+    alert(ani.sayName());
+}
+
+//window.onload = function (e) {
+//    loadMe(e);
+//    btnOver();
+//};
+
+window.onload = loadMe;
+
+function loadMe(e) {
+    e = e || window.event;
+    var btn = document.getElementById("btnA");
+    //alert(btn);
+    btn.onclick = function (e) {
+        e = e || window.event;
+        alert("I was clicked");
+    };
+
+    if (3 > 5) {
+        alert("big");
+        alert("something");
+    }
+
+    btnOver();
+
+    btn.onmouseenter = btnOver;
+    getSpan("para");
+}
+
+
+function btnOver() {
+    this.className = "over";
+}
+
+
+function getSpan(parID) {
+    var par = document.getElementById(parID);
+    var ul = par.childNodes[1];
+    var lis = ul.getElementsByTagName("li");
+    var spn = lis[1].childNodes[0];
+    return spn;
+
+}
