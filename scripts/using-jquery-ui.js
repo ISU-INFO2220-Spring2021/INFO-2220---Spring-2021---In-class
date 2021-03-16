42e8ed8425ae3c69526fd6907d439db0f568758c
Jon Holmes <holmjona@isu.edu>
2021-03-16 12:33:27 -0600

    Added jQuery UI and started demos of widgets.

---
+++ scripts/using-jquery-ui.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $("h3").draggable();
+
+    $("#divResizeMe").resizable();
+
+    $(".ordered").sortable();
+
+    $("#accordion").accordion({
+        heightStyle: "auto",
+        event: "mouseover"
+    });
+
+    var foods = [
+        "MoMos",
+        "Bread",
+        "Pizza",
+        "Chocolate",
+        "Lasagna",
+        "Chinese"
+    ];
+
+    $("#autocomplete").autocomplete({
+        source : foods
+    });
+
+    $("#datepicker").datepicker();
+
+});
