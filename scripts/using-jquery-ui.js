f83282bbacc8c3c5c3350f20c1aff1f5c0c2dea3
Jon Holmes <holmjona@isu.edu>
2021-03-18 12:36:27 -0600

    More jQueryUI discussion

--- scripts/using-jquery-ui.js
+++ scripts/using-jquery-ui.js
@@ -25,4 +25,51 @@
 
     $("#datepicker").datepicker();
 
+    $("#menu").menu({
+    
+    });
+
+    $("select").selectmenu();
+
+    $("#spinner").spinner({
+        
+    } );
+
+    $("#button").button();
+    $("#button-icon").button({
+        icon: "ui-icon-gear",
+        showLabel: false,
+        
+    });
+
+    $("#dialog-link").click(function myfunction() {
+        $("#dialog-message").dialog();
+    });
+
+    
+
+    //$("#tabs").tabs({
+    //    heightStyle: "auto"
+    //});
+
+    $("#tooltip").tooltip({
+        position: { my: "left+15 bottom-35", at: "top center" }
+    });
+
+    $("#spinner").tooltip({
+        position: { my: "left+100 top+20", at: "right top" }
+    });
+
+    $("input[type=radio]").checkboxradio();
+    $("input[type=checkbox]").checkboxradio();
+    $("#shadowBox").click(function () {
+       // $(this).hide();
+        $(this).hide({
+            duration: 5000,
+            easing: "swing"
+        })
+    });
+
+
+
 });
