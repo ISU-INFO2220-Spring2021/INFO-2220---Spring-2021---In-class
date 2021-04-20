45e9de5bb255f7c695d42301ae1628aa1b52937f
Jon Holmes <holmjona@isu.edu>
2021-04-20 12:13:30 -0600

    Added practice Final files. Talked more about storage.

---
+++ scripts/practScript.js
@@ -0,0 +1,66 @@
+﻿/// <reference path="jquery-3.1.1.js" />
+
+// Practice Test
+
+$(document).ready(function () {
+    $("#btnAny").click(function () {
+        alert($(this).attr("id"));
+    });
+
+    $("#spnClick").click(function () {
+        addSomething(this,"I was added!");
+    });
+
+    $("#btnMore").on("click", doThis);
+});
+
+function clearLinks() {
+    $("a").removeClass();
+}
+
+function addList() {
+    var div = $("<div>");
+    var ul = $("<ul>");
+    ul.append($("<li>").text("One"));
+    ul.append($("<li>").text("Two"));
+    ul.append($("<li>").text("Three"));
+    ul.append($("<li>").text("Four"));
+    ul.appendTo(div);
+    div.appendTo($("#myHolder"));
+}
+
+function changeClasses() {
+    $("p").addClass("textHolder");
+}
+
+function doThis() {
+    alert($(this).parent()[0].tagName);
+}
+
+function changeListText() {
+    $("li").eq(2).text("Text was changed.");
+
+}
+
+function addSomething(obj, str) {
+    var newP = $("<p>").text(str);
+    $(obj).parent().parent().append(newP);
+}
+
+function changeSpans() {
+    $("#someText span").append("- Added Here");
+}
+var toggle = true;
+function moveText() {
+    var p,t;
+    if (toggle) {
+        p = $("#myParagraph");
+        t = p.next();
+    } else {
+        p = $("#myParagraph").next();
+        t = p.prev();
+    }
+    toggle = !toggle;
+    t.text(p.text());
+    p.text("");
+}
--- scripts/storage2.js
+++ scripts/storage2.js
@@ -1,4 +1,8 @@
 var value = "hello";
+function Person(nme,age){
+    this.name = nme;
+    this.age = age;
+}
 if(typeof Storage == "undefined"){
     // we have no storage
     alert("your browser does not support this page. Please try another browser.")
@@ -17,19 +21,25 @@ if(typeof Storage == "undefined"){
         btnSave.onclick = function () {
             var txt = document.getElementById("txtName");
             //localStorage.name = txt.value;
-            sessionStorage.name = txt.value;
+            //sessionStorage.name = txt.value;
+            var pers = new Person(txt.value,35);
+            localStorage["person"] = JSON.stringify(pers);
         };
 
         var btnShow = document.getElementById("btnShowIt");
         btnShow.onclick = function () {
-            var name = sessionStorage.name;
+            var name = localStorage.name;
+            //var name = sessionStorage.name;
             value = name;
             alert(name);
+            var poe = JSON.parse(localStorage.person)
+            alert(poe.name);
             
         };
         var btnShowVal = document.getElementById("btnShowVariable");
         btnShowVal.onclick = function () {
-            alert(value);
+            //alert(value);
+            alert(localStorage.fruit)
         };
     };
 }
